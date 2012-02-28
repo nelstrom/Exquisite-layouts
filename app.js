697e428eae6c9fb74270e745a7378a1cb3842df0
Drew Neil <andrew.jr.neil@gmail.com>
2012-02-28 17:24:20 +0100

    Switch to a 'fit' layout.

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ Ext.application({
         var red = {
             style: "background-color: #B22222; color:white;",
             title: "Red",
-            html: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            html: "Red"
         };
 
         Ext.create('Ext.Container', {
             fullscreen: true,
-            layout: 'auto',
+            layout: 'fit',
             items: [red]
         });
 
