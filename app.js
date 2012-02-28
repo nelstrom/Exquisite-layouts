64adefc3a5b25f91ba79ad26f3d5382ad97f41ee
Drew Neil <andrew.jr.neil@gmail.com>
2012-02-28 17:21:14 +0100

    Create an auto-layout container with a red panel.

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 Ext.application({
     name: 'Sencha',
 
-    launch: function() {}
+    launch: function() {
+
+        var red = {
+            style: "background-color: #B22222; color:white;",
+            title: "Red",
+            html: "Red"
+        };
+
+        Ext.create('Ext.Container', {
+            fullscreen: true,
+            layout: 'auto',
+            items: [red]
+        });
+
+    }
 });
