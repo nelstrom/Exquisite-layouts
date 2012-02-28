5b048ecdb2f08fee73f93e07aa73b4ce0d243a35
Drew Neil <andrew.jr.neil@gmail.com>
2012-02-28 17:14:07 +0100

    Strip more boilerplate for an even blanker slate.

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 Ext.application({
     name: 'Sencha',
 
-    controllers: ['Main'],
-    views: ['Main'],
-
-    launch: function() {
-        Ext.Viewport.add({
-            xclass: 'Sencha.view.Main'
-        });
-    }
+    launch: function() {}
 });
